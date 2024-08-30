88c3507c8599d1152d91d6f5a7dad90926c11b2c
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-08-31 00:45:14 +0600

    problem 03

---
+++ 01. Basics Problems/problem_03.js
@@ -0,0 +1,12 @@
+/*
+Problem : You task is to divide the given number by 5 and show the remainder as the output.
+
+Output: Print the remainder.
+
+Example : Number: 119, Remainder: 4
+*/
+
+// Solution :
+let numbers = 119;
+let remainder = 119 % 5;
+console.log(remainder);
--- 01. Basics Problems/script - Copy (3).js
+++
@@ -1,7 +0,0 @@
-/*
-Problem :
-
-Example :
-*/
-
-// Solution :
