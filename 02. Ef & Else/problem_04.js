895057d1fd9ff23da67ec5e85173299ceeacbabf
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-08-31 14:41:17 +0600

    nested condition me & friend

--- 02. Ef & Else/problem_04.js
+++ 02. Ef & Else/problem_04.js
@@ -1,7 +1,28 @@
 /*
-Problem :
-
+Problem : if you get more then 80 then inside your friend score. 
+    
 Example :
+    If your friend get more than 80. then go for a lunch. 
+    if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+    if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+    if your friend get less than 40, block your friend
+    if you get less than 80 go to home and sleep and act sad.
 */
 
 // Solution :
+const myScore = 88;
+const friendScore = 78;
+
+if (myScore >= 80) {
+  if (friendScore >= 80) {
+    console.log("Go for a lunch");
+  } else if (friendScore < 80 && friendScore >= 60) {
+    console.log("Good luck next time.");
+  } else if (friendScore < 60 && friendScore >= 40) {
+    console.log("Friend's message unseen.");
+  } else {
+    console.log("Block your friend.");
+  }
+} else {
+  console.log("sleep and act sad.");
+}
