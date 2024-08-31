5dd0f30efe4ae32ea04ac3e323839668a0509284
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-08-31 15:52:51 +0600

    Ticket fare Calculator

--- 02. Ef & Else/problem_06.js
+++ 02. Ef & Else/problem_06.js
@@ -1,7 +1,29 @@
 /*
-Problem :
+Problem : Ticket fare Calculator
 
 Example :
+    - Children (age < 10): free
+    - Students get a 50% discount
+    - Senior citizens (age >= 60) gets a 15% Discount
+    - Otherwise Regular ticket fare 800 tk
 */
 
 // Solution :
+
+let ticketPrice = 800;
+let personAge = 65;
+let students = false;
+
+if (personAge < 10) {
+  console.log("Ticket Free");
+} else if (students) {
+  discountPrice = (ticketPrice * 50) / 100;
+  result = ticketPrice - discountPrice;
+  console.log("Ticket Price = " + result);
+} else if (personAge >= 60) {
+  discountPrice = (ticketPrice * 15) / 100;
+  result = ticketPrice - discountPrice;
+  console.log("Ticket Price = " + result);
+} else {
+  console.log("Ticket Price = " + ticketPrice);
+}
