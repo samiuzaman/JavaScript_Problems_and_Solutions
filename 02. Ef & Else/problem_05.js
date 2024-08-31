6fc60233bd9ecf997718685de5006210ec1b873d
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-08-31 15:32:42 +0600

    Ternary Number

--- 02. Ef & Else/problem_05.js
+++ 02. Ef & Else/problem_05.js
@@ -1,7 +1,23 @@
 /*
-Problem :
+Problem : you have two numbers in two variables, called: num1, num2
+
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. if not,then the value of the variable result will be the sum of num1 and num2.
 
 Example :
 */
 
 // Solution :
+const num1 = 15;
+const num2 = 8;
+let results;
+
+if (num1 > num2) {
+  console.log((results = num1 * 2));
+} else {
+  console.log(num1 + num2);
+}
+
+// using ternary operator
+results = num1 > num2 ? num1 * 2 : num1 + num2;
+console.log(results);
---
+++ 02. Ef & Else/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+if(num1 > num2) {
+    console.log(results = num1 * 2 );
+} else {
+    console.log(num1 + num2)
+}
