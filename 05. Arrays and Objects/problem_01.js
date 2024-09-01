4d490f0c5ac234836aa8f24d6e3ed09839a244da
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-01 14:50:13 +0600

    Declare an Array and Output in Array

---
+++ 05. Arrays and Objects/problem_01.js
@@ -0,0 +1,16 @@
+/*
+Problem : Declare an array
+
+Example :
+01. Declare an array with 5 elements containing fruits
+02. console log the 3rd index element
+03. change the value of the 2nd index element to jambura
+04. console log the final array
+
+*/
+
+// Solution :
+const fruits = ["Apple", "Banana", "Jackfruit", "Lychee", "Mango"];
+console.log(fruits[3]);
+fruits[2] = "Jambura";
+console.log(fruits)
