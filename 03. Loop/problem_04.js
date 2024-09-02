d32a1c6d325f16e43601a02a6e6e45232c9cea3a
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-02 14:43:57 +0600

    multiplication table for number

--- 03. Loop/problem_04.js
+++ 03. Loop/problem_04.js
@@ -1,7 +1,34 @@
 /*
 Problem :
+Subtask-1: Generate a multiplication table for number 9 (For Loop)?
+Subtask-2: As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5 (While Loop)?
 
 Example :
+9 * 1 = 9
+9 * 2 = 18 
+9 * 3 = 27 
+9 * 4 = 36 
+9 * 5 = 45 
+9 * 6 = 54 
+9 * 7 = 63 
+9 * 8 = 72 
+9 * 9 = 81 
+9 * 10 = 90
 */
 
 // Solution :
+// For Loop Problem
+let number = 9;
+for (let mul = 1; mul <= 10; mul++) {
+  const result = number * mul;
+  console.log(`${number} * ${mul} = ${result}`);
+}
+
+// While Loop Problem
+let number = 5;
+let mul = 1;
+while (mul <= 10) {
+  const result = number * mul;
+  console.log(`${number} * ${mul} = ${result}`);
+  mul++;
+}
