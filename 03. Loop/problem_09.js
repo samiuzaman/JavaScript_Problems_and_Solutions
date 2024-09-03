f2b644a084f0c6871076188559dc85bd4539a8c2
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-03 12:14:02 +0600

    sum upto 100 break

--- 03. Loop/problem_09.js
+++ 03. Loop/problem_09.js
@@ -1,7 +1,13 @@
 /*
-Problem :
-
-Example :
+Problem : Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
 
 // Solution :
+let sum = 0;
+for (let number = 1; number <= 100; number++) {
+  sum = sum + number;
+  if (sum >= 100) {
+    break;
+  }
+  console.log(sum);
+}
