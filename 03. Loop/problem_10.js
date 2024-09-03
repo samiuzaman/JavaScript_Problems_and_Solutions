2f59a710d74f04e4f6268a383fcfeda17cacbaad
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-03 16:05:18 +0600

    using break in first square number

--- 03. Loop/problem_10.js
+++ 03. Loop/problem_10.js
@@ -1,14 +1,14 @@
 /*
-Problem : Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number.
+Problem : Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc).
 
-Example : like 4, 9, 16, etc
+Example : 
 */
 
 // Solution :
 for (let number = 1; number <= 100; number++) {
   const squareNumber = number * number;
-  if (squareNumber < 100){
+  if(squareNumber === 4){
     console.log(squareNumber)
+    break;
   }
-  
 }
