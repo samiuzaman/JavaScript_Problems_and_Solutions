4f7a2a10a951c43cf90152a13e193d3793cb40b8
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-04 01:25:36 +0600

    get the even numbers from an array using any looping

--- 05. Arrays and Objects/problem_06.js
+++ 05. Arrays and Objects/problem_06.js
@@ -1,5 +1,5 @@
 /*
-Problem : Write a JavaScript code to get the even numbers from an array using any reverse looping technique.
+Problem : Write a JavaScript code to get the even numbers from an array using any looping technique.
 
 Example : 
 Input: const numbers = [12, 98, 5, 41, 23, 78, 46];
@@ -9,8 +9,10 @@ Output: [12, 98, 78, 46]
 
 // Solution :
 const numbers = [12, 98, 5, 41, 23, 78, 46];
-let reverseNumber = [];
+const evenNumbers = [];
 for (let num of numbers) {
-  reverseNumber.unshift(num);
+  if (num % 2 === 0) {
+    evenNumbers.push(num);
+  }
 }
-console.log(reverseNumber);
+console.log(evenNumbers);
