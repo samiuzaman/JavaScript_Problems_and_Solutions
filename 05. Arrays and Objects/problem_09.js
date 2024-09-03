c85e5134d9b2fee72b95bf65a4359a532e4c9ab2
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-04 01:38:22 +0600

    concatenate all elements of an array into a single string

---
+++ 05. Arrays and Objects/problem_09.js
@@ -0,0 +1,13 @@
+/*
+Problem : Use a for...of loop to concatenate all the elements of an array into a single string.
+
+Example : Input: const numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+
+Output: 'TomTimTinTik'
+*/
+
+// Solution :
+
+const numbers = ["Tom", "Tim", "Tin", "Tik"];
+const str = numbers.join("");
+console.log(str);
