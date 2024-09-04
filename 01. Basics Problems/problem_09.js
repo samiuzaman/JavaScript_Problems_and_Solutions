3788635b8173c61302cac3d9b2bb238145d8215c
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-04 12:44:06 +0600

    Check whether a string contains all the g, p, s, t

--- 01. Basics Problems/problem_09.js
+++ 01. Basics Problems/problem_09.js
@@ -3,4 +3,15 @@ Problem : Check whether a string contains all the g, p, s, t
 */
 
 // Solution :
+const strings =
+  "A programming language is programmers to communicate with computers.";
+let words = ["g", "p", "s", "w"];
+let allWords = true;
 
+for (let item = 0; item < words.length; item++) {
+  if (!strings.includes(words[item])) {
+    allWords = false;
+    break;
+  }
+}
+console.log(allWords)
