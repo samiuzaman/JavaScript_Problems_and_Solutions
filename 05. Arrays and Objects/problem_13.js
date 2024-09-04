8404f9af7fd097d91f519e39763644fa867bb2f3
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-04 16:14:47 +0600

    Display the physics marks as output.

--- 05. Arrays and Objects/problem_13.js
+++ 05. Arrays and Objects/problem_13.js
@@ -11,7 +11,17 @@ const student = {
         marks: 30
     }
 };
-};
 */
 
 // Solution :
+const student = {
+  name: "Hamim Sakep",
+  id: 5421,
+  physics: {
+    subject: "HSC Physics",
+    author: "Shahjahan Tapan",
+    marks: 30,
+  },
+};
+const result = student.physics.marks;
+console.log(result);
