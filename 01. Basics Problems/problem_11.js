1d1566720c8d74055cb43f67198ea3f4de64f94f
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-04 15:53:21 +0600

    Capitalize Every first Letter of each word in a String

--- 01. Basics Problems/problem_11.js
+++ 01. Basics Problems/problem_11.js
@@ -3,3 +3,11 @@ Problem : Capitalize Every first Letter of each word in a String
 */
 
 // Solution :
+let text = "You should be able to check whether x exists";
+
+text = text.toLowerCase().split(" ");
+for (let item = 0; item < text.length; item++) {
+  text[item] = text[item].charAt(0).toUpperCase() + text[item].slice(1);
+}
+const result = text.join(" ");
+console.log(result);
