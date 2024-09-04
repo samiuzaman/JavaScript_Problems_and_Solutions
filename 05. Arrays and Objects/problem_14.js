fb5240822c41d8ddc9afdfecf1c0d75076186897
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-04 16:20:29 +0600

    Count the number of Object properties

--- 05. Arrays and Objects/problem_14.js
+++ 05. Arrays and Objects/problem_14.js
@@ -13,3 +13,12 @@ Output: 4
 */
 
 // Solution :
+let student = {
+  name: "Ariana Grande",
+  age: 21,
+  city: "Gaibandha",
+  isStudent: true,
+};
+
+const count = Object.keys(student).length;
+console.log(count);
