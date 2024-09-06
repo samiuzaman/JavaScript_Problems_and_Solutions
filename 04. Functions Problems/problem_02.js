61f5442b8eb6a5de26c8fa029b0b2fb59e0913bf
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-06 16:40:30 +0600

    number is odd multiply it by 2 or number is even divide it by 2

--- 04. Functions Problems/problem_02.js
+++ 04. Functions Problems/problem_02.js
@@ -1,7 +1,16 @@
 /*
-Problem :
-
-Example :
+Problem : Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
 */
 
 // Solution :
+function multiplyOrDivide(number) {
+  if (number % 2 === 0) {
+    const mul = number / 2;
+    return mul;
+  } else {
+    const div = number * 2;
+    return div;
+  }
+}
+const result = multiplyOrDivide(6);
+console.log(result);
