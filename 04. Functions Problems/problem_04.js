135719288f42900a0287ad95632753f9856eb0fb
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-06 17:08:50 +0600

    Count the binary string 0

--- 04. Functions Problems/problem_04.js
+++ 04. Functions Problems/problem_04.js
@@ -1,7 +1,20 @@
 /*
-Problem :
-
-Example :
+Problem : Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
 */
 
 // Solution :
+const binaryString = "10011010011";
+
+function count_zero(str) {
+  const strLength = str.length;
+  let count = 0;
+  for (let i = 0; i <= strLength; i++) {
+    if (str[i] === "0") {
+      count++;
+    }
+  }
+  return count;
+}
+
+const result = count_zero(binaryString);
+console.log("Total 0 is = ", result);
