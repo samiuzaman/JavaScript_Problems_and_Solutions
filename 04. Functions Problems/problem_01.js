cc8d843e5613cb87e87fbabc13466aef532bfdfb
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-06 16:33:49 +0600

    Multiply the four numbers and then return the result

---
+++ 04. Functions Problems/problem_01.js
@@ -0,0 +1,11 @@
+/*
+Problem : Take four parameters. Multiply the four numbers and then return the result
+*/
+
+// Solution :
+function multiplication(a, b, c, d) {
+  const mul = a * b * c * d;
+  return mul;
+}
+const result = multiplication(2, 4, 6, 8);
+console.log(result);
