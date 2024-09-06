d955e8a19ae531ee7713fe35e72dfeab4f82042e
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-06 17:19:20 +0600

    If even return String Even. If odd return String  Odd

--- 04. Functions Problems/problem_05.js
+++ 04. Functions Problems/problem_05.js
@@ -1,7 +1,16 @@
 /*
-Problem :
-
-Example :
+Problem : Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd.
 */
 
 // Solution :
+function odd_even(num) {
+  const number = parseInt(num);
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+const result = odd_even(6);
+console.log(result);
