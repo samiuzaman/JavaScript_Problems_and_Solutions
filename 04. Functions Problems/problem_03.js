a73556acb9bdc3db807e71d5ba0e25e19de54e25
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-06 16:53:06 +0600

    array return the average of those values

--- 04. Functions Problems/problem_03.js
+++ 04. Functions Problems/problem_03.js
@@ -1,7 +1,17 @@
 /*
-Problem :
-
-Example :
+Problem : Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
 */
 
 // Solution :
+const numbers = [12, 41, 24, 18, 37, 19];
+function make_avg(allNumbers) {
+  let numbersLength = allNumbers.length;
+  let sum = 0;
+  for (let number of allNumbers) {
+    sum += number;
+  }
+  return sum / numbersLength;
+}
+
+const result = make_avg(numbers);
+console.log(result);
