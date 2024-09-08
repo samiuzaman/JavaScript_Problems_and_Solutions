8aad82eebee70dda0e073ca67a3222d7f6a47866
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-09 00:58:32 +0600

    Calculate the average of the odd Numbers in an Array

--- 01. Basics Problems/problem_14.js
+++ 01. Basics Problems/problem_14.js
@@ -1,5 +1,24 @@
 /*
-Problem : Get first name, last name form user and print fullname. Print Total length of full name. Convert full name into uppercase print the 2nd position from full name.
+Problem : Calculate the average of the odd Numbers in an Array?
 */
 
 // Solution :
+const numbers = [42, 13, 58, 65, 81, 96, 7, 5];
+
+function average(numbers) {
+  const odds = [];
+  for (let number of numbers) {
+    if (number % 2 !== 0) {
+      odds.push(number);
+    }
+  }
+
+  let sum = 0;
+  for (let number of odds) {
+    sum += number;
+  }
+  const averageNumber = sum / odds.length;
+  return averageNumber;
+}
+const result = average(numbers);
+console.log(result);
