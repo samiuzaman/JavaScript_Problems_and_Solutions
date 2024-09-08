2fc2d4e7252026896a95f02918a4f7164c9042e1
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-09 00:26:34 +0600

    convert inci to feet

--- 01. Basics Problems/problem_13.js
+++ 01. Basics Problems/problem_13.js
@@ -1,5 +1,17 @@
 /*
-Problem : Get first name, last name form user and print fullname. Print Total length of full name. Convert full name into uppercase print the 2nd position from full name.
+Problem : Convert Inci to Feet
+
+Example: 
+input: 68
+output : 5 ft 8 inci 
 */
 
 // Solution :
+function inciToFeet(inci) {
+  const feet = parseInt(inci / 12);
+  const remainInci = inci % 12;
+  return `${feet} ft ${remainInci} inci`;
+}
+
+const result = inciToFeet(68);
+console.log(result);
