3dc62ede0493a648ecea9c8e70e764281d96addb
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-09 14:20:58 +0600

    Finding  2nd Largest Number

--- 01. Basics Problems/problem_23.js
+++ 01. Basics Problems/problem_23.js
@@ -1,7 +1,21 @@
 /*
-Problem :
+Problem : Finding 2nd largest integer number of an Array.
 
 Example :
 */
 
 // Solution :
+const marks = [73, 65, 88, 68, 94, 76, 82];
+function secondLargestNumber(numbers) {
+  let largestNumber = numbers[0];
+  let secondLargestNumber = numbers[0];
+  for (let number of numbers) {
+    if (largestNumber < number) {
+      secondLargestNumber = largestNumber;
+      largestNumber = number;
+    }
+  }
+  return secondLargestNumber;
+}
+const result = secondLargestNumber(marks);
+console.log(result);
