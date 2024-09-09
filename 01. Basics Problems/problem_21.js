238fcfe274c6375a2fbfff7c37b36e1e6ec18a2c
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-09 14:03:23 +0600

    Finding 1st largest number

--- 01. Basics Problems/problem_21.js
+++ 01. Basics Problems/problem_21.js
@@ -1,7 +1,18 @@
 /*
-Problem :
-
-Example :
+Problem : Finding 1st largest integer number of an Array.
 */
 
 // Solution :
+const marks = [73, 65, 88, 68, 94, 76, 82];
+function fundLargestNumber(numbers) {
+  let largestNumber = numbers[0];
+  for (let number of numbers) {
+    if (largestNumber < number) {
+      largestNumber = number;
+    }
+  }
+  return largestNumber;
+}
+
+const result = fundLargestNumber(marks);
+console.log(result);
