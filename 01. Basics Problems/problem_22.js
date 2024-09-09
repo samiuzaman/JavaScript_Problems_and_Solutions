ae6e0df832bd9baabdbac6b82b9199083bb4a716
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-09 14:08:53 +0600

    Finding 1st smallest number

--- 01. Basics Problems/problem_22.js
+++ 01. Basics Problems/problem_22.js
@@ -1,7 +1,20 @@
 /*
-Problem :
+Problem : Finding 1st smallest number of an Array.
 
 Example :
 */
 
 // Solution :
+const marks = [73, 65, 88, 68, 94, 76, 82];
+
+function findSmallestNumber(numbers) {
+  let smallestNumber = numbers[0];
+  for (let number of numbers) {
+    if (smallestNumber > number) {
+      smallestNumber = number;
+    }
+  }
+  return smallestNumber;
+}
+const result = findSmallestNumber(marks);
+console.log(result);
--- 01. Basics Problems/tempCodeRunnerFile.js
+++ 01. Basics Problems/tempCodeRunnerFile.js
@@ -1,1 +1,17 @@
-x
+/*
+Problem : Finding 1st smallest number of an Array.
+
+Example :
+*/
+
+// Solution :
+const marks = [73, 65, 88, 68, 94, 76, 82];
+
+function findSmallestNumber(numbers) {
+  let smallestNumber = numbers[0];
+  for(let number of numbers){
+    console.log(number)
+  }
+}
+const result = findSmallestNumber(marks);
+console.log(result);
