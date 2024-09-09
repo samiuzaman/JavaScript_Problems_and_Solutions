e010c13cc0769fb0c0d5b4676bc42945e19147a5
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-09 14:29:27 +0600

    Finding 2nd smallest number

--- 01. Basics Problems/problem_24.js
+++ 01. Basics Problems/problem_24.js
@@ -1,7 +1,21 @@
 /*
-Problem :
-
-Example :
+Problem : Finding 2st smallest number of an Array.
 */
 
 // Solution :
+const marks = [73, 65, 88, 68, 94, 76, 82];
+function find2ndSmallestNumber(numbers) {
+  let smallestNumber = numbers[0];
+  let secondSmallestNumber = numbers[0];
+  for (let number of numbers) {
+    if (smallestNumber > number) {
+      secondSmallestNumber = smallestNumber;
+      smallestNumber = number;
+    } else if (secondSmallestNumber > number) {
+      secondSmallestNumber = number;
+    }
+  }
+  return secondSmallestNumber;
+}
+const result = find2ndSmallestNumber(marks);
+console.log(result);
