09910b375cc807bde668227b2c1d35faf6a759e6
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-09 23:15:12 +0600

    sum of all product prices is in an array of objects.

--- 01. Basics Problems/problem_27.js
+++ 01. Basics Problems/problem_27.js
@@ -1,7 +1,25 @@
 /*
-Problem :
+Problem : The sum of all product prices is in an array of objects.
 
 Example :
 */
 
 // Solution :
+
+const products = [
+  { name: "shampoo", price: 300, quantity: 5 },
+  { name: "chiruni", price: 100, quantity: 3 },
+  { name: "shirt", price: 700, quantity: 2 },
+  { name: "pant", price: 1200, quantity: 2 },
+];
+
+function sumOfObject(products) {
+  let sum = 0;
+  for (let product of products) {
+    const productCost = product.price * product.quantity;
+    sum = sum + productCost;
+  }
+  return sum;
+}
+const result = sumOfObject(products);
+console.log(result);
