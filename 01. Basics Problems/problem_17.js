ebe99d80fa5215f19e2512924f8fc796071874ef
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-09 12:22:30 +0600

    count the number of vowels in a string

--- 01. Basics Problems/problem_17.js
+++ 01. Basics Problems/problem_17.js
@@ -1,7 +1,23 @@
 /*
-Problem :
-
-Example :
+Problem : Write a function to count the number of vowels in a string.
 */
 
 // Solution :
+const str = "Programming Language";
+function countVowels(str) {
+  let count = 0;
+  for (let letter = 0; letter < str.length; letter++) {
+    if (
+      str[letter] === "a" ||
+      str[letter] === "e" ||
+      str[letter] === "i" ||
+      str[letter] === "o" ||
+      str[letter] === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+const result = countVowels(str);
+console.log(result);
