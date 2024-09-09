045f84dbcae3ac1cb46159bb9e7728b366346b40
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-09 19:27:40 +0600

    add and multiplication to calculate shirt, pant and shoe

--- 01. Basics Problems/problem_25.js
+++ 01. Basics Problems/problem_25.js
@@ -1,7 +1,24 @@
 /*
-Problem :
+Problem :  Use add and multiplication to calculate Shirts, Pants and Shoes requirements;
 
 Example :
+shirt price --> 500 tk
+pant price --> 600 tk
+shoe price --> 900 tk
 */
 
 // Solution :
+function totalPrice(shirtQuantity, pantQuantity, shoeQuantity) {
+  const shirtPrice = 500;
+  const pantPrice = 600;
+  const shoePrice = 900;
+
+  const totalShirtPrice = shirtPrice * shirtQuantity;
+  const totalPantPrice = pantPrice * pantQuantity;
+  const totalShoePrice = shoePrice * shoeQuantity;
+
+  const allPrice = totalShirtPrice + totalPantPrice + totalShoePrice;
+  return allPrice;
+}
+const result = totalPrice(2, 3, 1);
+console.log(result);
