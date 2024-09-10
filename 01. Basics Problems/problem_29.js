0b8f4a0c523951d70e5a61a2ecb9c6d1f4a8bb27
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-10 12:33:49 +0600

    calculate the total budget required to buy electronics

--- 01. Basics Problems/problem_29.js
+++ 01. Basics Problems/problem_29.js
@@ -1,7 +1,29 @@
 /*
-Problem :
+Problem : Your task is to calculate the total budget required to buy electronics:
 
-Example :
+    laptop = 35000 tk
+    tablet = 15000 tk
+    mobile = 20000 tk
+
+Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required.
 */
 
 // Solution :
+function calculateElectronicsBudget(
+  laptopQuality,
+  tabletQuality,
+  mobileQuality
+) {
+  const laptop = 35000;
+  const tablet = 15000;
+  const mobile = 20000;
+
+  const totalLaptopCost = laptop * laptopQuality;
+  const totaltabletCost = tablet * tabletQuality;
+  const totalMobileCost = mobile * mobileQuality;
+
+  const totalCost = totalLaptopCost + totaltabletCost + totalMobileCost;
+  return totalCost;
+}
+const result = calculateElectronicsBudget(1, 2, 3);
+console.log(result);
